ff24e5665104a23a127e4f513f9af9ae9db7300b
Jooyon Won <jyon1204@gmail.com>
2019-05-08 21:24:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,8 @@
 def reformat_languages(languages)
-  # your code here
+  new_langugages = {}
+  languages.each do |style, language|
+    languages.each do |name, attributes|
+      new_langugages[name]|| = attributes
+      new_langugages[name][:style] = []
+      new_langugages[name][:style] << style
 end
