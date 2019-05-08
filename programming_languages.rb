0b613f616146384f22b740f6b3fc5dbe4f4f3045
Jooyon Won <jyon1204@gmail.com>
2019-05-08 21:36:42 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reformat_languages(languages)
   new_langugages = {}
   languages.each do |style, language|
-    languages.each do |type, status|
+    language.each do |type, status|
       binding.pry
       new_langugages[type]= status
       new_langugages[type][:style] = []
